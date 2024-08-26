085ac697ab38846088b773f989d59576086b6d2d
Benedikt Becker <benedikt@remixlabs.com>
2024-08-25 21:48:29 -0400

    forgotten mapping tab

---
+++ src/main/frontend/src/app/mapping/mapping-tab/mapping-tab.component.ts
@@ -0,0 +1,55 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CsvImport, Indexing, Mapping, Start, StartType, ServerInfo, Vocabularies } from '../data';
+import { ApiService } from '../api.service';
+import * as ops from '../mapping-ops';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'mapping-tab',
+  templateUrl: './mapping-tab.component.html',
+  styleUrls: ['./mapping-tab.component.scss']
+})
+export class MappingTabComponent {
+  @Input() mappingShortkey! : string | null;
+  @Input() mapping! : Mapping;
+  @Input() serverInfo! : ServerInfo;
+  @Input() vocabularies! : Vocabularies;
+  @Input() canDownload! : boolean;
+  @Output() run = new EventEmitter<ops.Operation>();
+
+  constructor(
+    private api : ApiService,
+    private snackBar : MatSnackBar,
+  ) { }
+
+  isIndexing(start : Start) : start is Indexing {
+    return start != null && start.type == StartType.Indexing
+  }
+
+  isCsvImport(start : Start) : start is CsvImport {
+    return start != null && start.type == StartType.CsvImport
+  }
+
+  remap() {
+    if (this.serverInfo.umlsVersion != null) {
+      (async () => {
+        let { conceptsCodes, vocabularies, messages } =
+          await this.api.remapData(this.mapping, this.vocabularies, this.mapping.meta);
+        if (messages.length) {
+          this.snackBar.open(messages.join("\n\n"), "Ok", { panelClass: 'remap-snackbar' });
+        }
+        this.run.emit(new ops.Remap(this.serverInfo.umlsVersion, conceptsCodes, vocabularies));
+      })();
+    } else {
+      console.error("unknown UMLS version");
+    }
+  }
+
+  download(mappingShortkey : string, includeDescendants : boolean) {
+    let url = new URL(this.api.downloadMappingUrl);
+    url.searchParams.set('mappingShortkey', mappingShortkey);
+    url.searchParams.set('includeDescendants', "" + includeDescendants);
+    url.searchParams.set('url', window.location.href);
+    window.open(url, '_blank');
+  }
+}
